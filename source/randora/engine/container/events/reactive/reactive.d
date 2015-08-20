f96f175de9e4a26996ec3aba241ea38c1b6f9edf
Master-Foo <joshka.randora@gmail.com>
2015-08-20 00:22:45 -0600

    Refactor trivial RNDContainer Reactive Events to to use templates for purposes of DRY

--- source/randora/engine/container/events/reactive/reactive.d
+++ source/randora/engine/container/events/reactive/reactive.d
@@ -1,19 +1,32 @@
 module randora.engine.container.events.reactive.reactive;
 
+template ReactiveEvent(string var_name){
+	static const char[] ReactiveEvent = "
+	override void on_"~var_name~"(){
+		super.on_"~var_name~"();
+		foreach(int i, RNDWidget!(ObjectType) slave; this.slaves){
+			slave."~var_name~"();
+		}
+	}
+	";
+}
+
 import randora.engine.container.events.reactive;
 mixin template Reactive(){
-	mixin Clean;
-	mixin Clear;
-	mixin Draw;
-	mixin Event;
-	mixin Init;
-	mixin Load;
+	mixin(ReactiveEvent!("clean"));
+	mixin(ReactiveEvent!("clear"));
+	mixin(ReactiveEvent!("draw"));
+	mixin(ReactiveEvent!("event"));
+	mixin(ReactiveEvent!("init"));
+	mixin(ReactiveEvent!("load"));
+	//mixin(ReactiveEvent!("log"));
+	mixin(ReactiveEvent!("loop"));
+	mixin(ReactiveEvent!("render"));
+	mixin(ReactiveEvent!("resize"));
+	mixin(ReactiveEvent!("sort"));
+	mixin(ReactiveEvent!("start"));
+	mixin(ReactiveEvent!("update"));
+	
 	mixin Log;
-	mixin Loop;
 	mixin Print;
-	mixin Render;
-	mixin Resize;
-	mixin Sort;
-	mixin Start;
-	mixin Update;
 }
--- source/randora/engine/container/events/reactive/resize.d
+++
@@ -1,6 +0,0 @@
-module randora.engine.container.events.reactive.resize;
-
-import randora.engine.container.events.reactive;
-mixin template Resize(){
-
-}
