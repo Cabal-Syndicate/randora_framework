a25bdb7b5a371c35242a9085349a77234492b852
Master-Foo <joshka.randora@gmail.com>
2015-08-19 23:53:04 -0600

    Refactor trivial RNDObject Reactive Events to to use templates for purposes of DRY

--- source/randora/engine/object/events/reactive/package.d
+++ source/randora/engine/object/events/reactive/package.d
@@ -2,17 +2,6 @@ module randora.engine.object.events.reactive;
 
 package import randora.engine.object.events.reactive.reactive;
 
-package import randora.engine.object.events.reactive.clean;
-package import randora.engine.object.events.reactive.clear;
-package import randora.engine.object.events.reactive.draw;
-package import randora.engine.object.events.reactive.event;
-package import randora.engine.object.events.reactive.init;
-package import randora.engine.object.events.reactive.load;
 package import randora.engine.object.events.reactive.log;
-package import randora.engine.object.events.reactive.loop;
 package import randora.engine.object.events.reactive.print;
-package import randora.engine.object.events.reactive.render;
-package import randora.engine.object.events.reactive.resize;
-package import randora.engine.object.events.reactive.start;
-package import randora.engine.object.events.reactive.sort;
-package import randora.engine.object.events.reactive.update;
+
--- source/randora/engine/object/events/reactive/reactive.d
+++ source/randora/engine/object/events/reactive/reactive.d
@@ -1,19 +1,49 @@
 module randora.engine.object.events.reactive.reactive;
 
+template ReactiveEvent(string var_name){
+	static const char[] ReactiveEvent = "
+	public bool "~var_name~"able	= true;
+	public bool "~var_name~"ed		= false;
+	public bool "~var_name~"ing		= false;
+	
+	void pre_"~var_name~"(){}
+	
+	void on_"~var_name~"(){
+		foreach(int i, RNDObject member; this.members){
+			member."~var_name~"();
+		}
+	}
+	
+	void post_"~var_name~"(){}
+	
+	void "~var_name~"(){
+		if(this."~var_name~"able){
+			this."~var_name~"ing = true;
+			this.pre_"~var_name~"();
+			this.on_"~var_name~"();
+			this.post_"~var_name~"();
+			this."~var_name~"ing = false;
+		}
+		this."~var_name~"ed = true;
+	}
+	";
+}
+
 mixin template Reactive(){
+	mixin(ReactiveEvent!("clean"));
+	mixin(ReactiveEvent!("clear"));
+	mixin(ReactiveEvent!("draw"));
+	mixin(ReactiveEvent!("event"));
+	mixin(ReactiveEvent!("init"));
+	mixin(ReactiveEvent!("load"));
+	mixin(ReactiveEvent!("loop"));
+	mixin(ReactiveEvent!("render"));
+	mixin(ReactiveEvent!("resize"));
+	mixin(ReactiveEvent!("sort"));
+	mixin(ReactiveEvent!("start"));
+	mixin(ReactiveEvent!("update"));
+	
 	import randora.engine.object.events.reactive;
-	mixin RNDClean;
-	mixin RNDClear;
-	mixin RNDDraw;
-	mixin RNDEvent;
-	mixin RNDInit;
-	mixin RNDLoad;
-	mixin RNDLog;
-	mixin RNDLoop;
 	mixin RNDPrint;
-	mixin RNDRender;
-	mixin RNDResize;
-	mixin RNDSort;
-	mixin RNDStart;
-	mixin RNDUpdate;
+	mixin RNDLog;
 }
