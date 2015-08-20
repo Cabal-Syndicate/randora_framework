301a274283a2531ca639324314978f1315f26e9d
Master-Foo <joshka.randora@gmail.com>
2015-08-20 00:25:53 -0600

    asdf

--- source/randora/engine/container/events/reactive/package.d
+++ source/randora/engine/container/events/reactive/package.d
@@ -4,4 +4,3 @@ package import randora.engine.container.events.reactive.reactive;
 
 package import randora.engine.container.events.reactive.log;
 package import randora.engine.container.events.reactive.print;
-
